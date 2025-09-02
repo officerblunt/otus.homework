6224db7581e078a75c44a7c8c902eb519e967e26
nuvvanda <officerblunt@gmail.com>
2025-09-02 12:26:22 +0300

    initial

---
+++ Otus/AsyncTasks/Homework.cs
@@ -0,0 +1,69 @@
+﻿namespace PlayGround.Otus.AsyncTasks;
+
+public static class Homework
+{
+    private const string FOLDER_PATH = @"C:\Users\alex\projects\PlayGround\PlayGround";
+
+    public static async Task Main(string[] args)
+    {
+        var files = new[]
+        {
+            Path.Combine(FOLDER_PATH, "test1.txt"),
+            Path.Combine(FOLDER_PATH, "test2.txt"),
+            Path.Combine(FOLDER_PATH, "test3.txt")
+        };
+
+        var tasks = files.Select(CountSpacesInFileAsync).ToArray();
+
+        var results = await Task.WhenAll(tasks);
+
+        for (var i = 0; i < files.Length; i++)
+        {
+            Console.WriteLine($"{files[i]} contains {results[i]} whitespaces.");
+        }
+
+        var folderResults = await CountSpacesInFolderAsync(FOLDER_PATH);
+
+        foreach (var kvp in folderResults)
+        {
+            Console.WriteLine($"{kvp.Key} contains {kvp.Value} whitespaces.");
+        }
+    }
+
+    private static async Task<int> CountSpacesInFileAsync(string path)
+    {
+        if (!File.Exists(path)) throw new FileNotFoundException(path);
+
+        try
+        {
+            var content = await File.ReadAllTextAsync(path);
+
+            var count = content.Count(c => c == ' ');
+            return count;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error reading file {path}: {ex.Message}");
+            return 0;
+        }
+    }
+
+    private static async Task<Dictionary<string, int>> CountSpacesInFolderAsync(string folderPath)
+    {
+        if (!Directory.Exists(folderPath))
+        {
+            Console.WriteLine($"Folder {folderPath} not found.");
+            return new();
+        }
+
+        var files = Directory.GetFiles(folderPath);
+        var tasks = files.Select(async file =>
+        {
+            var count = await CountSpacesInFileAsync(file);
+            return (file, count);
+        }).ToArray();
+
+        var results = await Task.WhenAll(tasks);
+        return results.ToDictionary(r => r.file, r => r.count);
+    }
+}
---
+++ Otus/AsyncTasks/Parallelism.cs
@@ -0,0 +1,92 @@
+﻿
+namespace PlayGround.Otus.AsyncTasks;
+
+public static class Parallelism
+{
+    private const string RESULT_FILE_NAME = "results.txt";
+    public static void Main()
+    {
+        int[] sizes = [100000, 1000000, 10000000];
+        var random = new Random();
+
+        foreach (var size in sizes)
+        {
+            Console.WriteLine($"Array length: {size}");
+            File.AppendAllText(RESULT_FILE_NAME, $"Array length: {size}\n");
+
+            var array = GenerateArray(size, random);
+
+            Measure("Synchronously", () => CountSynchronously(array));
+            Measure("Thread", () => CountWithThreads(array));
+            Measure("PLINQ", () => CountByLinq(array));
+        }
+    }
+
+    private static int[] GenerateArray(int size, Random random)
+    {
+        var arr = new int[size];
+        for (var i = 0; i < size; i++)
+        {
+            arr[i] = random.Next(1, 100);
+        }
+
+        return arr;
+    }
+
+    private static void Measure(string message, Func<int> action)
+    {
+        var now = DateTime.Now;
+        action();
+        var result = $"({(DateTime.Now - now).TotalMilliseconds})\t [{Environment.CurrentManagedThreadId}] | {message}";
+        Console.WriteLine(result);
+        File.AppendAllText(RESULT_FILE_NAME, $"{result}\n");
+    }
+
+    private static int CountSynchronously(int[] array)
+    {
+        var result = 0;
+        foreach (var t in array)
+        {
+            result += t;
+        }
+
+        return result;
+    }
+
+    private static int CountByLinq(int[] array)
+    {
+        return array.AsParallel().Sum();
+    }
+
+    private static int CountWithThreads(int[] array)
+    {
+        const int numberOfThreads = 4;
+        var chunkSize = array.Length / numberOfThreads;
+
+        var partialSums = new int[numberOfThreads];
+        var threads = new List<Thread>();
+
+        for (var i = 0; i < numberOfThreads; i++)
+        {
+            var threadIndex = i;
+            threads.Add(new Thread(() =>
+            {
+                var localSum = 0;
+                var start = threadIndex * chunkSize;
+                var end = (threadIndex == numberOfThreads - 1) ? array.Length : start + chunkSize;
+
+                for (var j = start; j < end; j++)
+                {
+                    localSum += array[j];
+                }
+
+                partialSums[threadIndex] = localSum;
+            }));
+        }
+
+        foreach (var t in threads) t.Start();
+        foreach (var t in threads) t.Join();
+
+        return partialSums.Sum();
+    }
+}
